b24bc0d4988157ebe1ce10cca27f5ea5fcdd1f74
kavan-livebird <kavan@livebirdtechnologies.com>
2026-06-25 13:44:20 +0530

    transform

--- apps/gps/urls.py
+++ apps/gps/urls.py
@@ -1,26 +1,18 @@
 from django.contrib import admin
-
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
-
-
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-
     path('', include('converter.urls')),
-
     path('api/', include('gdal_server.urls')),
-
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui-root'),
-
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
